ff040ad7579610429ee3da0e2dcef6361031f9f3
VallentinX <valentin.cristian_98@icloud.com>
2024-03-22 02:33:13 +0200

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,145 @@
+import React, { Component } from "react";
+
+import Searchbar from "./Searchbar/SearchBar.jsx";
+import ImageGallery from "./ImageGallery/ImageGallery.jsx";
+import Button from "./Button/Button.jsx";
+import Modal from "./Modal/Modal.jsx";
+import Loader from "./Loader/Loader.jsx";
+
+import { getImgs } from "./Api/Api.js";
+
+import "./App.module.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      query: "",
+      page: 1,
+      loading: false,
+      largeImageURL: "",
+      showModal: false,
+      hasMoreImages: true,
+    };
+  }
+
+  handleSearch = (newQuery) => {
+    this.setState({
+      query: newQuery,
+      page: 1,
+      images: [],
+      lastSearch: newQuery,
+    });
+  };
+
+  handleInitLoad = () => {
+    this.getImgs();
+  };
+
+  handleLoadMore = () => {
+    const { hasMoreImages, lastSearch } = this.state;
+
+    if (!hasMoreImages) {
+      return;
+    }
+
+    this.setState(
+      (prevState) => ({
+        page: prevState.page + 1,
+        loading: true,
+      }),
+      () => {
+        if (lastSearch) {
+          this.fetchImages(lastSearch, this.state.page);
+        } else {
+          this.handleInitLoad();
+        }
+      }
+    );
+  };
+
+  handleImageClick = (url) => {
+    this.setState({
+      largeImageURL: url,
+      showModal: true,
+    });
+  };
+
+  handleCloseModal = () => {
+    this.setState({
+      largeImageURL: "",
+      showModal: false,
+    });
+  };
+
+  componentDidMount() {
+    if (this.state.page === 1 && this.state.query) {
+      this.getImgs();
+    }
+    this.handleInitLoad();
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { query, page, loading } = this.state;
+
+    if (prevState.query === query && prevState.page === page) {
+      return;
+    }
+
+    if (!loading) {
+      this.getImgs();
+    }
+  }
+
+  getImgs = async (query = this.state.query, page = this.state.page) => {
+    try {
+      const newImages = await getImgs(query, page);
+
+      this.setState((prevState) => ({
+        images: [
+          ...prevState.images,
+          ...newImages.map((image) => ({
+            ...image,
+            alt: image.tags || "Image",
+          })),
+        ],
+        hasMoreImages: newImages.length === 12,
+      }));
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    const { images, loading, hasMoreImages, largeImageURL, showModal } =
+      this.state;
+
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSearch} />
+
+        <ImageGallery images={images} onImageClick={this.handleImageClick} />
+
+        {loading && <Loader />}
+
+        {hasMoreImages && (
+          <Button onClick={this.handleLoadMore} show={hasMoreImages}>
+            Load more
+          </Button>
+        )}
+
+        {showModal && (
+          <Modal
+            onClose={this.handleCloseModal}
+            largeImageURL={largeImageURL}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
